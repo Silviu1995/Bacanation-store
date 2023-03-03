0ed34b8416b593488f903f4e77b27212a884b368
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2023-03-03 17:55:34 +0200

    : added more responsiveness. comiting to test

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,28 +4,32 @@ import './Categories.scss'
 const Categories = () => {
   return (
     <div className='categories'>
-        <div className="col">
-            <div className="row">
-                <img src="https://images.pexels.com/photos/15337886/pexels-photo-15337886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="col col-l">
+        <div className="row">
+                <img src="https://images.pexels.com/photos/7035508/pexels-photo-7035508.jpeg" alt="" />
                 <button>
-                    <Link className='link'>Sales</Link>
+                    <Link className='link'>Women</Link>
                 </button>
-            </div>
+            </div> 
             <div className="row">
-                <img src="https://images.pexels.com/photos/15355473/pexels-photo-15355473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <button>
-                    <Link className='link'>Sales</Link>
-                </button>
-            </div>
-        </div>
-        <div className="col">
-            <div className="row">
-                <img src="https://images.pexels.com/photos/14565975/pexels-photo-14565975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <button>
-                    <Link className='link'>Sales</Link>
-                </button>
-            </div>
-        </div> 
+                <div className="col">
+                    <div className="row">
+                    <img src="https://images.pexels.com/photos/15290928/pexels-photo-15290928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                    <button>
+                        <Link className='link'>Men</Link>
+                    </button>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="row">
+                        <img src="https://images.pexels.com/photos/1619815/pexels-photo-1619815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <button>
+                            <Link className='link'>Kids</Link>
+                        </button>
+                    </div>
+                </div>
+            </div>   
+        </div>  
         <div className="col col-l">
             <div className="row">
                 <div className="col">
