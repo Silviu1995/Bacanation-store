bb90a10dba6753427b7f481e2352800c73603016
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2023-03-05 21:57:32 +0200

    :redesigned categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -8,7 +8,7 @@ const Categories = () => {
         <div className="row">
                 <img src="https://images.pexels.com/photos/7035508/pexels-photo-7035508.jpeg" alt="" />
                 <button>
-                    <Link className='link'>Women</Link>
+                    <Link to='/products/2' className='link'>Women</Link>
                 </button>
             </div> 
             <div className="row">
@@ -16,7 +16,7 @@ const Categories = () => {
                     <div className="row">
                     <img src="https://images.pexels.com/photos/15290928/pexels-photo-15290928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                     <button>
-                        <Link className='link'>Men</Link>
+                        <Link to='/products/1' className='link'>Men</Link>
                     </button>
                     </div>
                 </div>
@@ -24,37 +24,11 @@ const Categories = () => {
                     <div className="row">
                         <img src="https://images.pexels.com/photos/1619815/pexels-photo-1619815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                         <button>
-                            <Link className='link'>Kids</Link>
+                            <Link to='/products/3' className='link'>Kids</Link>
                         </button>
                     </div>
                 </div>
             </div>   
-        </div>  
-        <div className="col col-l">
-            <div className="row">
-                <div className="col">
-                    <div className="row">
-                    <img src="https://images.pexels.com/photos/15290928/pexels-photo-15290928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                    <button>
-                        <Link className='link'>Sales</Link>
-                    </button>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="row">
-                        <img src="https://images.pexels.com/photos/15296759/pexels-photo-15296759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                        <button>
-                            <Link className='link'>Sales</Link>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <img src="https://images.pexels.com/photos/2475255/pexels-photo-2475255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <button>
-                    <Link className='link'>Sales</Link>
-                </button>
-            </div>    
         </div>  
     </div>
   )
